47742f6d7bff326d0e7c4851910e378c4d795deb
CoreFloDev <CoreFloDev@users.noreply.github.com>
2020-04-25 18:05:12 +0200

    simplify layout

--- app/src/main/java/dev/zarah/databinding/BannersActivity.kt
+++ app/src/main/java/dev/zarah/databinding/BannersActivity.kt
@@ -1,11 +1,8 @@
 package dev.zarah.databinding
 
 import android.os.Bundle
-import android.view.View
-import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import dev.zarah.databinding.databinding.ActivityBannersBinding
 
@@ -22,28 +19,19 @@ class BannersActivity : AppCompatActivity() {
 
     enum class Banner(
         val label: String,
-        @ColorRes val highlight: Int,
-        @ColorRes val background: Int
+        @DrawableRes val background: Int
     ) {
         INFO(
             "Information",
-            R.color.banner_info_highlight,
-            R.color.banner_info_background
+            R.drawable.banner_info_background
         ),
         WARNING(
             "Warning",
-            R.color.banner_warning_highlight,
-            R.color.banner_warning_background
+            R.drawable.banner_warning_background
         ),
         ERROR(
             "Error",
-            R.color.banner_error_highlight,
-            R.color.banner_error_background
+            R.drawable.banner_error_background
         )
     }
-}
-
-@BindingAdapter("bannerHighlight")
-fun setBannerHighlight(view: View, banner: BannersActivity.Banner) {
-    view.setBackgroundColor(ContextCompat.getColor(view.context, banner.highlight))
 }
